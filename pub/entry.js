f604e581fd73014705e2289676a4e90feaca9ee5
r-edw <aaabbc1@hotmail.co.uk>
2016-01-06 22:02:34 +0000

    adding react related tuff

--- pub/entry.js
+++ pub/entry.js
@@ -1,2 +1,9 @@
-require("./css/main.css");
-document.write(require("./js/main.js"));
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./js/main.jsx";
+
+main();
+
+function main() {
+  ReactDOM.render(<Main />, document.getElementById("content"))
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,29 @@
+var path = require('path');
+
 module.exports = {
-    entry: "./pub/entry.js",
+    entry: [
+        "webpack/hot/dev-server",
+        "webpack-dev-server/client?http://localhost:8080",
+        path.resolve(__dirname, "./pub/entry.js")
+      ],
     output: {
-        path: __dirname,
-        filename: "dist/bundle.js"
+        path: path.resolve(__dirname, 'build'),
+        filename: "bundle.js"
     },
     module: {
         loaders: [
-            { test: /\.css$/, loader: "style!css" }
+            {
+              test: /\.css$/,
+              loader: "style!css"
+            },
+            {
+              test: /\.jsx?$/,
+              exclude: /node_modules/,
+              loader: "babel",
+              query: {
+                presets: ['es2015', 'react']
+              }
+            }
         ]
     }
 };
