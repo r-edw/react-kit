352ac5a96a2892633a292440ff1f9a33bef77096
r-edw <aaabbc1@hotmail.co.uk>
2016-01-07 20:12:35 +0000

    adding linting

--- src/entry.js
+++ src/entry.js
@@ -1,9 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Main from "./js/main.js";
+import ReactDOM from 'react-dom'
+import Main from './js/main.js'
 
-main();
+main()
 
 function main() {
-  ReactDOM.render(<Main />, document.getElementById("content"))
+    ReactDOM.render(<Main />, document.getElementById('content'))
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import './../css/main.scss';
+import React from 'react'
+import './../css/main.scss'
 
 export default class Hello extends React.Component{
-  render() {
-    return <h1>Hello From React Component!</h1>
-  }
+    render() {
+        return <h1>Hello From React Component!</h1>
+    }
 }
